ffa930825db201df3e959685b0e9cc38a29ba2e3
GGastoNN <65984801+GGastoNN@users.noreply.github.com>
2026-09-21 14:23:55 -0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.illu.relevametal"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.5.0"
+        versionCode = 3
+        versionName = "0.6.0"
     }
 
     buildFeatures { compose = true }
