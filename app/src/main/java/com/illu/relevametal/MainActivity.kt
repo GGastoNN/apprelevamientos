8a6e9c5a0fbda12ad75b442196e4742548452e91
GGastoNN <65984801+GGastoNN@users.noreply.github.com>
2026-09-23 20:50:29 -0300

    Add files via upload

--- app/src/main/java/com/illu/relevametal/MainActivity.kt
+++ app/src/main/java/com/illu/relevametal/MainActivity.kt
@@ -21,7 +21,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GrupoIdeaTheme {
+            val personalization by vm.personalization.collectAsState()
+            GrupoIdeaTheme(personalization) {
                 App(vm = vm)
             }
         }
